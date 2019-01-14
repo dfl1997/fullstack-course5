e915c3e3edc9b86e0ff6c19308e4dc3838886246
dfl1997 <dfl@yahoo.com>
2019-01-14 14:49:07 -0800

    my changes

--- examples/Lecture05/beginning-of-lecture-code/app.js
+++ examples/Lecture05/beginning-of-lecture-code/app.js
@@ -1,10 +1,11 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('myFirstApp', [])
+    angular.module('myFirstApp', [])
 
-.controller('MyFirstController', function () {
-
-});
+        .controller('MyFirstController', function ($scope) {
+            $scope.name = "foo";
+            $scope.name1 = "bar"
+        });
 
 })();
